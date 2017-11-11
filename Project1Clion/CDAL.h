a5d649de2e556c4dac141e25481af5ed5cedd9f9
CalamariDude <zeineddine.jad@gmail.com>
2017-11-11 14:20:56 -0500

    startfresh commits

---
+++ Project1Clion/CDAL.h
@@ -0,0 +1,458 @@
+//
+// Created by Jad Zeineddine on 9/19/17.
+//
+
+#ifndef PROJECT1CLION_CDAL_H
+#define PROJECT1CLION_CDAL_H
+
+#include "List.h"
+namespace cop3530 {
+    template<typename E>
+    struct carNode {
+    public:
+        carNode *next;
+        node *cars[50];
+
+        carNode() {
+            next == nullptr;
+        }
+    };
+
+    template<typename E>
+    class CDAL : public List<E> {
+    public:
+        int head = -2;
+        int tail = -2;
+        carNode *root = nullptr;
+
+        // adds the specified element to the list at the specified position, shifting
+        // the element originally at that and those in subsequent positions one
+        // position to the ”right.“
+        void insert(E element,
+                    size_t position) override;
+
+        // appends the specified element to the list.
+        void push_back(E element) override;
+
+        // prepends the specified element to the list.
+        void push_front(E element) override;
+
+        // replaces the existing element at the specified position with the specified
+        // element and return the original element.
+        void replace(E element,
+                     size_t position) override;
+
+        // removes and returns the the element at the specified position, shifting the
+        // subsequent elements one position to the ”left.“
+        E remove(size_t position) override;
+
+        // removes and returns the element at the list's tail.
+        E pop_back(void) override;
+
+        // removes and returns the element at the list's head.
+        E pop_front(void) override;
+
+        // returns (without removing from the list) the element at the specified
+        // position.
+        E item_at(size_t position) override;
+
+        // returns the element at the list's tail.
+        E peek_back(void) override;
+
+        // returns the element at the list's head.
+        E peek_front(void) override;
+
+        // returns true IFF the list contains no elements.
+        bool is_empty(void) override;
+
+        // returns true IFF the no more elemtns can be added to the list.
+        bool is_full(void) override;
+
+        // returns the number of elements in the list as a size_t.
+        size_t length(void) override;
+
+        // removes all elements from the list.
+        void clear(void) override;
+
+        // returns true IFF at least one of the elements of the list matches the
+        // specified element.
+        bool contains(E element,
+                      bool (*equals_to_function)(E, E)) override;
+
+        // If the list is empty, inserts "<empty list>" into the ostream; otherwise,
+        // inserts, enclosed in square brackets, the list's elements, separated by
+        // commas, in sequential order.
+        void print(std::ostream &os) override;
+
+        // Allocates, initializes, and returns an array containing a copy of the
+        // list's elements in sequential order.
+        E *contents(void) override;
+
+    private:
+        //helper functions
+        carNode * carnode_at(int index);
+
+        int get_free_node_service();
+
+        void first_element_insert(E element);
+
+        E remove_last_element();
+
+        void move_up(int position);
+
+        void move_back(int position);
+
+        void deallocate_unused();
+        //end of helper functions
+        //        ~CDAL() override;
+    };
+
+    template<typename E>
+    CDAL<E>::CDAL() {
+        root = new carNode();
+    }
+
+    template<typename E>
+    void CDAL<E>::insert(E element, size_t position) {
+        if (is_empty() || position >= length()) {
+            throw std::runtime_error(
+                    "cannot remove in empty list or out of bounds"
+            );
+        }
+        if(position == length()){
+            push_back(element);
+        }
+        else if(position == 0) {
+            push_front(element);
+        }
+        else{
+            move_up_from(position);
+            carNode * node = carnode_at(position);
+            node->cars[position%50] = element;
+        }
+    }
+
+    template<typename E>
+    void CDAL<E>::push_back(E element) {
+
+        if(is_empty()){
+            first_element_insert(element);
+        }
+        int indexToAdd = get_free_node_service();
+        carNode * carnode = carnode_at(indexToAdd);
+        carnode->cars[indexToAdd%50] = element;
+        tail = indexToAdd;
+
+    }
+
+    template<typename E>
+    void CDAL<E>::push_front(E element) {
+        if(is_empty()){
+            first_element_insert(element);
+        }
+        move_up(0);
+        carNode * node = carnode_at(0);
+        node->cars[0] = element;
+        head = 0;
+    }
+
+    template<typename E>
+    void CDAL<E>::replace(E element,
+                          size_t position) {
+        if (is_empty() || position >= length()) {
+            throw std::runtime_error(
+                    "cannot replace in empty list or out of bounds"
+            );
+        }
+
+        carNode *carnode = carnode_at(position);
+        carnode->cars[position % 50] = element;
+    }
+
+    template<typename E>
+    E CDAL<E>::remove(size_t position) {
+        if (is_empty() || position >= length()) {
+            throw std::runtime_error(
+                    "cannot remove in empty list or out of bounds"
+            );
+        }
+        E element;
+        if (position == length()) {
+            elemet = pop_back();
+        } else if (position == 0) {
+            element = pop_front();
+        }
+        carNode node = carnode_at(position);
+        element = node->cars[position%50];
+        move_back(position);
+        deallocate_unused();
+        return element;
+    }
+
+    template<typename E>
+    E CDAL<E>::pop_back(void) {
+        if (is_empty() || position >= length()) {
+            throw std::runtime_error(
+                    "cannot remove in empty list or out of bounds"
+            );
+        }
+
+        if(length() == 1){
+            return remove_last_element();
+        }
+
+        carNode * node = carnode_at(tail);
+        E element = node[tail%50];
+        tail--;
+        deallocate_unused();
+        return element;
+    }
+
+    template<typename E>
+    E CDAL<E>::pop_front(void) {
+        if (is_empty()) {
+            throw std::runtime_error(
+                    "cannot remove in empty list or out of bounds"
+            );
+
+        }
+        if(length() == 1){
+            return remove_last_element();
+        }
+        //head should always stays at 0.
+        carNode * node = carnode_at(head);
+        E element = node[head%50];
+        move_back(head);
+        deallocate_unused();
+        return element;
+    }
+
+    template<typename E>
+    E CDAL<E>::item_at(size_t position) {
+        if (is_empty() || position >= length()) {
+            throw std::runtime_error(
+                    "cannot remove in empty list or out of bounds"
+            );
+
+        }
+
+        carNode * node = carnode_at(position);
+        return node[position%50];
+    }
+
+    template<typename E>
+    E CDAL<E>::peek_back(void) {
+        if (is_empty() || position >= length()) {
+            throw std::runtime_error(
+                    "cannot remove in empty list or out of bounds"
+            );
+        }
+
+        carNode * node = carnode_at(tail);
+        return node[tail%50];
+
+    }
+
+    template<typename E>
+    E CDAL<E>::peek_front(void) {
+        if (is_empty() || position >= length()) {
+            throw std::runtime_error(
+                    "cannot remove in empty list or out of bounds"
+            );
+        }
+
+        carNode * node = carnode_at(head);
+        return node[head%50];
+    }
+
+    template<typename E>
+    bool CDAL<E>::is_empty(void) {
+        if(length() == 0 ){
+            return true;
+        }
+        return false;
+    }
+
+    template<typename E>
+    bool CDAL<E>::is_full(void) {
+        return false;
+    }
+
+    template<typename E>
+    size_t CDAL<E>::length(void) {
+        int it = head;
+        carNode * node = carnode_at(it);
+        int counter = 0;
+        if(head != -2  && tail != -2)
+            return tail-head + 1;
+        else
+            return 0;
+    }
+
+    template<typename E>
+    void CDAL<E>::clear(void) {
+        carNode * node = carnode_at(head);
+        int it = head;
+        tail = head ;
+        deallocate_unused();
+        head = tail = -2;
+    }
+
+    template<typename E>
+    bool CDAL<E>::contains(E element,
+                           bool (*equals_to_function)(E, E)) {
+        if (is_empty() || position >= length()) {
+            throw std::runtime_error(
+                    "cannot remove in empty list or out of bounds"
+            );
+        }
+
+        int it = head;
+        carNode * node = carNode_at(it);
+        while(it != tail + 1){
+            if(node->cars[it%50] == element){
+                return true;
+            }
+            it++;
+            node = carnode_at(it);
+        }
+    }
+
+    template<typename E>
+    void CDAL<E>::print(std::ostream &os) {
+        if (is_empty() || position >= length()) {
+            throw std::runtime_error(
+                    "cannot remove in empty list or out of bounds"
+            );
+        }
+
+        int it = head;
+        carNode * node = carNode_at(it);
+        while(it != tail + 1){
+            os << node[it%50] << " " ;
+            it++;
+            node = carnode_at(it);
+        }
+    }
+
+    template<typename E>
+    E *CDAL<E>::contents(void) {
+        int length = length();
+        E * arr[length];
+        size_t counter = 0;
+        int it = head;
+        carNode * node = carNode_at(it);
+        while(it != tail + 1){
+            arr[counter] = node[it%50];
+            it++;
+            counter++;
+            node = carnode_at(it);
+        }
+        return arr;
+    }
+
+
+    //helper functions
+    template<typename E>
+    carNode * carnode_at(int index){
+        int timestogo = index/50;
+        carNode * it = root;
+        while(timestogo != 0 ){
+            it = it->next;
+            timestogo--;
+        }
+        return it;
+    }
+
+    template<typename E>
+    int get_free_node_service() {
+        //check if next index is the first element of next array
+        if(is_empty()){
+         return 0;
+        }
+        if((tail+1) % 50 == 0 ){
+            if(!carnode_at(tail)->next){
+                carNode node = new carNode();
+                carnode_at(tail)->next = node;
+            }
+        }
+        return tail+1;
+    }
+
+
+    template<typename E>
+    void first_element_insert(E element){
+        head = tail = 0;
+        root[head] = element;
+    }
+
+    template<typename E>
+    E remove_last_element(){
+        E element = carnode_at(head)[head%50];
+        head = tail = -2;
+        return element;
+    }
+
+    template<typename E>
+    void move_up(int position){
+
+        int it = get_free_node_service()-1; //=tail but allocates new array if needed
+        carNode * node = carnode_at(it);
+        carNode * move_up_node;
+
+
+        while(it >= position){
+            node = carnode_at(it);
+            move_up_node[(it+1)%50] = node[it%50];
+            move_up_node = node;
+            it--;
+        }
+    }
+
+    template<typename E>
+    void move_back(int position){
+        int it = position + 1;
+        carNode * node = carnode_at(it);
+        carNode * move_back_node;
+        while(it <= tail){
+            node = carnode_at(it);
+            move_back_node[(it-1) % 50] = node[it%50];
+            move_back_node = node;
+            it++;
+        }
+        tail--;
+    }
+
+    template<typename E>
+    void deallocate_unused(){
+        int used = (tail - head)/50;
+        int unused = 0;
+        carNode * node = carnode_at(tail);
+        while(node->next){
+            unused++;
+            node = node->next;
+        }
+        if(unused/used >= 1){
+            int todeallocate = unused/2;
+        }
+        node = carnode_at(tail);
+        while(todeallocate != 0){
+            node = node->next;
+            todeallocate--;
+        }
+        node->next = nullptr;
+    }
+
+//end of helper functions
+
+//    template<typename E>
+//    CDAL<E>::~CDAL() {}
+
+}
+
+
+
+
+
+
+#endif //PROJECT1CLION_CDAL_H
---
+++ Project1Clion/main.cpp
@@ -0,0 +1,143 @@
+#ifndef _MAIN_CPP
+#define _MAIN_CPP
+#define DEBUG
+ #include "List.h"
+ #include "SSLL.h"
+ #include "PSLL.h"
+ #include "SDAL.h"
+// #include "CDAL.h"
+#include <iostream>
+#include <vector>
+
+// #include <cstdio>
+
+using namespace cop3530;
+std::vector<int> a(100);
+std::vector<int> b(20);
+std::vector<int> c(75);
+
+void setup(List<int> *list) {
+    list->clear();
+
+}
+
+bool push_pop(List<int> *list) {
+    setup(list);
+    std::cout << "push_pop" << std::endl;
+    list->push_back(5);
+    list->push_back(5);
+    list->push_back(5);
+    list->push_back(5);
+    int i = list->pop_back();
+    int j = list->pop_front();
+    return i == 5 && j == 5;
+}
+
+bool push_insert_remove(List<int> *list)    {
+    setup(list);
+    std::cout << "push_insert_remove" << std::endl;
+    list->push_back(5);
+    list->push_front(5);
+    list->insert(5, 1);
+    int i = list->remove(1);
+    return i==5;
+}
+
+ bool push_peek_print(List<int> *list)       {
+     setup(list);
+     std::cout << "push_peek_print" << std::endl;
+     list->push_back(5);
+     int i = list->peek_back();
+     int j = list->peek_front();
+     list->print(std::cout);
+     return i==j;
+ }
+
+ bool push_clear_push(List<int> *list)       {
+     setup(list);
+     std::cout << "push_clear_push" << std::endl;
+     list->push_back(5);
+     list->push_back(5);
+     list->push_back(5);
+     list->clear();
+     return list->is_empty();
+ }
+
+ bool push_replace_pop(List<int> *list)      {
+     setup(list);
+     std::cout << "push_replace_pop" << std::endl;
+     list->push_back(5);
+     list->push_back(5);
+     list->push_back(5);
+     list->replace(5, 1);
+     return 5 == list->item_at(1);
+ }
+
+ bool push_replace_contents(List<int> *list) {
+     setup(list);
+     std::cout << "push_replace_contents" << std::endl;
+     list->push_back(5);
+     list->replace(5, 0);
+     int *arr = list->contents();
+     return arr[0] == 5;
+ }
+
+ bool push_length_isempty(List<int> *list)   {
+     std::cout << "push_length_isempty" << std::endl;
+     setup(list);
+//     std::cout<<"________________________________________________"<<std::endl;
+//     list->printVector();
+//     std::cout<<"________________________________________________"<<std::endl;
+     list->clear();
+     list->push_back(5);
+     std::cout<<"__________________________________________"<<std::endl;
+     list->printVector();
+     list->pop_back();
+     std::cout<<"__________________________________________"<<std::endl;
+     list->printVector();
+
+     return(list->is_empty() || list->length( ) == 0);
+ }
+
+int main() {
+//     List<int> *ssll = new SSLL<int>();
+//     List<int> *psll = new PSLL<int> ();
+     List<int> * sdal = new SDAL<int> ();
+    // List<int> *cdal = new CDAL();
+    std::vector<List<int> > lists;
+//    lists.push_back(ssll);
+//     lists.push_back(psll);
+     lists.push_back(sdal);
+    // lists.push_back(cdal);
+    for (int i = 0; i < lists.size(); i++) {
+         bool push_pop_test           = push_pop(lists.at(i));
+         bool push_insert_remove_test = push_insert_remove(lists.at(i));
+         bool push_peek_print_test       = push_peek_print(lists.at(i));
+         bool push_clear_push_test       = push_clear_push(lists.at(i));
+         bool push_replace_pop_test      = push_replace_pop(lists.at(i));
+         bool push_replace_contents_test = push_replace_contents(lists.at(i));
+         bool push_length_isempty_test   = push_length_isempty(lists.at(i));
+//        std::cout << "list num " << i << std::endl;
+//        std::cout << "push_pop_test = " << push_pop_test << std::endl;
+//        std::cout << "push_insert_remove_test = " << push_insert_remove_test <<
+//                  std::endl;
+//         std::cout << "push_peek_print_test = "  << push_peek_print_test <<
+//         std::endl;
+//         std::cout << "push_clear_push_test = " << push_clear_push_test <<
+//         std::endl;
+//         std::cout << "push_replace_pop_test = " << push_replace_pop_test  <<
+//           std::endl;
+//         std::cout << "push_replace_contents_test = " << push_replace_contents_test
+//         <<
+//           std::endl;
+         std::cout << "push_length_isempty_test = " << push_length_isempty_test << std::endl;
+        std::cout << std::endl;
+        if (push_pop_test && push_insert_remove_test && push_peek_print_test  && push_clear_push_test  && push_replace_pop_test  && push_replace_contents_test && push_length_isempty_test){
+            std::cout << "all tests passed for list " << i << std::endl;
+        }
+    }
+
+    return 0;
+}
+
+#endif // ifndef _TEST_CPP
